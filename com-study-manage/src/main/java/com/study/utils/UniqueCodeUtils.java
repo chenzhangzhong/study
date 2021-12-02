4972af75357a1b935265f7f9156ede4b3b66b26c
chenzhangzhong <2398354041@qq.com>
2021-12-02 16:47:20 +0800

    feat: 增加唯一码工具类

---
+++ com-study-manage/src/main/java/com/study/utils/UniqueCodeUtils.java
@@ -0,0 +1,103 @@
+package com.study.utils;
+
+import java.util.Random;
+
+/**
+ * @author chenzhangzhong
+ * @date 2021/11/22
+ * @description 唯一码工具类
+ */
+public class UniqueCodeUtils {
+    /**
+     * 自定义进制(0,1没有加入,容易与o,l混淆)，数组顺序可进行调整增加反推难度，A用来补位因此此数组不包含A，共31个字符。
+     */
+    private static final char[] BASE = new char[]{'H', 'V', 'E', '8', 'S', '2', 'D', 'Z', 'X', '9', 'C', '7', 'P',
+            '5', 'I', 'K', '3', 'M', 'J', 'U', 'F', 'R', '4', 'W', 'Y', 'L', 'T', 'N', '6', 'B', 'G', 'Q'};
+
+    /**
+     * A补位字符，不能与自定义重复
+     */
+    private static final char SUFFIX_CHAR = 'A';
+
+    /**
+     * 进制长度
+     */
+    private static final int BIN_LEN = BASE.length;
+
+    /**
+     * 生成唯一码最小长度
+     */
+    private static final int CODE_LEN = 6;
+
+    /**
+     * id转换为唯一码
+     *
+     * @param id 唯一id（这里用的数据库的主键id）
+     * @return
+     */
+    public static String idToCode(Long id) {
+        char[] buf = new char[BIN_LEN];
+        int charPos = BIN_LEN;
+
+        // 当id除以数组长度结果大于0，则进行取模操作，并以取模的值作为数组的坐标获得对应的字符
+        while (id / BIN_LEN > 0) {
+            int index = (int) (id % BIN_LEN);
+            buf[--charPos] = BASE[index];
+            id /= BIN_LEN;
+        }
+
+        buf[--charPos] = BASE[(int) (id % BIN_LEN)];
+        // 将字符数组转化为字符串
+        String result = new String(buf, charPos, BIN_LEN - charPos);
+
+        // 长度不足指定长度则随机补全
+        int len = result.length();
+        if (len < CODE_LEN) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(SUFFIX_CHAR);
+            Random random = new Random();
+            // 去除SUFFIX_CHAR本身占位之后需要补齐的位数
+            for (int i = 0; i < CODE_LEN - len - 1; i++) {
+                sb.append(BASE[random.nextInt(BIN_LEN)]);
+            }
+
+            result += sb.toString();
+        }
+
+        return result;
+    }
+
+    /**
+     * 唯一码解析出ID
+     * 基本操作思路恰好与idToCode反向操作。
+     *
+     * @param code 需要解析的唯一码
+     * @return
+     */
+    public static Long codeToId(String code) {
+        char[] charArray = code.toCharArray();
+        long result = 0L;
+        for (int i = 0; i < charArray.length; i++) {
+            int index = 0;
+            for (int j = 0; j < BIN_LEN; j++) {
+                if (charArray[i] == BASE[j]) {
+                    index = j;
+                    break;
+                }
+            }
+
+            if (charArray[i] == SUFFIX_CHAR) {
+                break;
+            }
+
+            if (i > 0) {
+                result = result * BIN_LEN + index;
+            } else {
+                result = index;
+            }
+        }
+
+        return result;
+
+    }
+}
